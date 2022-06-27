5dbc1c04083fc404587b1919b55a7b611dd090c2
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-06-27 19:57:15 +0100

    React-component structure completed

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from "./Header.module.css";
+
+function Header() {
+  return (
+    <header>
+
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/Components/Title/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import css from "./Title.module.css";
+
+function Title() {
+  return <div>Title</div>;
+}
+
+export default Title;
