28c9473ad900d2dcf41b4e72aac73aa8010e7ce3
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-06-27 20:19:12 +0100

    set header and footer

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import Title from "../Title";
+import SelectionButton from "../SelectionButton";
 import css from "./Header.module.css";
 
 function Header() {
   return (
     <header>
-
+      <Title />
+      <SelectionButton />
     </header>
-  )
+  );
 }
 
 export default Header;
