7288a2f1dd8e019876f91fbe08a83c92c5ccf65e
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-06-27 21:06:05 +0100

    getData function created and the data is comming with different endpoints, tested with the console

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import css from "./App.module.css";
 import Header from "../Header";
 import Main from "../Main";
 import Footer from "../Footer";
+import randomNumber from "../../HelperFunctions";
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
   const [endPoint, setEndPoint] = useState("posts");  // State to keep the endpoints of the url for making the data requests.
   const [buttonClicked, setButtonClicked] = useState(true);  // State for triggering useEffect every time is clicked. 
   const [data, setData] = useState(null);  // State to save the state to show on the main component.
-  console.log(endPoint,buttonClicked);
+  console.log(data);
 
   // Function to toggle the value of the state everytime the button is clicked.
   function handleClickButton() {
@@ -21,6 +22,22 @@ function App() {
   function handleChange(e) {
     setEndPoint(e.target.value);
   }
+  
+  // useEffec for sending the Fetch request to the API.
+  useEffect(() => {
+    // Create the url with the endpoints and the random number getting from the randomBumber function.
+    const url = "https://jsonplaceholder.typicode.com/";
+    let apiUrl = `${url}${endPoint}/${randomNumber(endPoint)}`;
+    // Function for getting the data from the API.
+    async function getData() {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setData(data);
+    }
+    if (endPoint) {
+      getData();
+    }
+  },[endPoint, buttonClicked])
 
   return (
     <div className={css.App}>
